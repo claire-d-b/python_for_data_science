1e94c3d392f003a87775b5805413e26d45d8debc
claire-d-b <claire.deberny@essca.eu>
2024-06-15 11:10:20 +0200

    handle multi-args case

--- ex05/building.py
+++ ex05/building.py
@@ -1,57 +1,58 @@
 from sys import argv
 
-def set_args(args, kwargs):
+def main():
+# your tests and your error handling
+
     key = 0
+    args = []
+    kwargs = {}
+    if len(argv[1:]) == 0:
+        while True:
+            try:
+                args = input()
+            except EOFError:
+                break
     for arg in argv[1:]:
         value = arg
         kwargs[key] = value
         args.append(arg)
         key += 1
 
-def main():
-# your tests and your error handling
-    args = []
-    kwargs = {}
-
-    set_args(args, kwargs)
-    try:
-        assert len(args) <= 2, "more than one argument is provided"
-    except AssertionError as e:
-            print(f"AssertionError: {e}")
-
-    finally:
-        punctuation = {'.', ',', ';', ':', '!', '?', '\'', '"', '(', ')', '[', ']', '{', '}', '-', '_', '/', '\\', '|', '@', '#', '$', '%', '^', '&', '*', '<', '>', '~', '`'}
+    punctuation = {'.', ',', ';', ':', '!', '?', '\'', '"', '(', ')', '[', ']', '{', '}', '-', '_', '/', '\\', '|', '@', '#', '$', '%', '^', '&', '*', '<', '>', '~', '`'}
+    if len(argv[1:]) > 0:
         print("The text contains " + str(len(*args)) + " characters: ")
-        i = 0
-        for arg in args:
-            for item in arg:
-                if item.isupper():
-                    i += 1
-        print(str(i) + " upper letters")
-        i = 0
-        for arg in args:
-            for item in arg:
-                if item.islower():
-                    i += 1
-        print(str(i) + " lower letters")
-        i = 0
-        for arg in args:
-            for item in arg:
-                if item in punctuation:
-                    i += 1
-        print(str(i) + " punctuation marks")
-        i = 0
-        for arg in args:
-            for item in arg:
-                if item.isspace():
-                    i += 1
-        print(str(i) + " spaces")
-        i = 0
-        for arg in args:
-            for item in arg:
-                if item.isdigit():
-                    i += 1
-        print(str(i) + " digits")
+    else:
+        print("The text contains " + str(len(args)) + " characters: ")
+    i = 0
+    for arg in args:
+        for item in arg:
+            if item.isupper():
+                i += 1
+    print(str(i) + " upper letters")
+    i = 0
+    for arg in args:
+        for item in arg:
+            if item.islower():
+                i += 1
+    print(str(i) + " lower letters")
+    i = 0
+    for arg in args:
+        for item in arg:
+            if item in punctuation:
+                i += 1
+    print(str(i) + " punctuation marks")
+    i = 0
+    for arg in args:
+        for item in arg:
+            if item.isspace():
+                i += 1
+    print(str(i) + " spaces")
+    i = 0
+    for arg in args:
+        for item in arg:
+            if item.isdigit():
+                i += 1
+    print(str(i) + " digits")
 
 if __name__ == "__main__":
     main()
