af580290c3b3d3d099bde7daa8f55041df179c44
claire-d-b <claire.deberny@essca.eu>
2024-06-25 11:07:27 +0200

    Add ex08 progress bar

---
+++ ex08/src/Loading.py
@@ -0,0 +1,8 @@
+from tqdm import tqdm
+
+def ft_tqdm(lst: range) -> None:
+    # Using the generator function
+    i = 0
+    for i in tqdm(lst, total=len(lst), leave=True, unit='it'):
+        yield
+
